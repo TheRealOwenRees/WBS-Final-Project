f8a0c28e9115627fe12a7ee127e0b843ae47b212
Owen Rees <owenrees82@gmail.com>
2023-02-10 21:29:10 +0100

    initial commit

---
+++ draft/gulpfile.js
@@ -0,0 +1,25 @@
+const { series, parallel, watch, src, dest } = require('gulp');
+const nodemon = require('gulp-nodemon');
+const browserSync = require('browser-sync');
+
+function startNodemon() {
+    nodemon({
+      script: 'server.js',
+      ext: 'js',
+      ignore: 'node_modules',
+      env: {'NODE_ENV': 'development' },
+    })
+  }
+
+function browserReload(cb) {
+  browserSync.reload()
+  cb();
+}
+
+exports.default = () => {
+  browserSync.init({
+    proxy: 'http://localhost:5000'
+  })
+  watch(['views/*.ejs', 'public/css/*.css'], browserReload)
+  startNodemon();
+}
---
+++ draft/server.js
@@ -0,0 +1,174 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const fileupload = require('express-fileupload');
+const stream = require('stream');
+
+const app = express();
+const port = 5000;
+
+// template engine EJS
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileupload());
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/game', (req, res) => {
+  const initialConfig = {
+    width: '300px',
+    resizable: false,
+    position: 'start',
+    showCoords: false,
+    orientation: 'white',
+    theme: 'default',
+    pieceStyle: 'wikipedia',
+    figurine: '',
+    locale: 'en',
+    timerTime: '',
+    layout: 'top',
+    showFen: false,
+    coordsInner: true,
+    headers: false,
+    coordsFactor: '1.0',
+    coordsFontSize: '',
+    colorMarker: '',
+    startPlay: '',
+    hideMovesBefore: true,
+    notation: 'short',
+    notationLayout: 'inline',
+  };
+  res.render('game', {
+    config: initialConfig,
+    event: '',
+    site: '',
+    date: '',
+    round: '',
+    white: '',
+    black: '',
+    result: '',
+    eco: '',
+    whiteElo: '',
+    blackElo: '',
+    plyCount: '',
+    eventDate: '',
+    source: '',
+  })
+});
+
+app.post('/game', (req, res) => {
+
+  // create a PGN file in string format, ready to write/convert
+  const createPgnString = (data) => {
+    return (
+      `[Event "${data.event}"]
+[Site "${data.site}"]
+[Date "${data.date}"]
+[Round "${data.round}"]
+[White "${data.white}"]
+[Black "${data.black}"]
+[Result "${data.result}"]
+[ECO "${data.eco}"]
+[WhiteElo "${data.whiteElo}"]
+[BlackElo "${data.blackElo}"]
+[PlyCount "${data.plyCount}"]
+[EventDate "${data.eventDate}"]
+[Source "${data.source}"]
+
+${data.moves}`
+    )
+  }
+
+  // save loaded game as PDF
+  if (req.body.action === 'savepgn') {
+    const pgnString = createPgnString(req.body)
+
+    let fileContents = Buffer.from(pgnString, 'utf-8');
+    let readStream = new stream.PassThrough();
+    readStream.end(fileContents);
+
+    res.set('Content-disposition', 'attachment; filename=' + 'game.pgn');
+    res.set('Content-Type', 'text/plain');
+
+    readStream.pipe(res);
+    readStream.end()
+  }
+
+  // load game and split PGN
+  if (req.body.action === 'loadpgn') {
+    try {
+      const pgnData = req.files.pgnfile.data.toString()
+      const pgnHeader = pgnData.split(/\n\n/g)[0]
+      const event = pgnHeader.match(/(?<=Event.").*(?=")/)[0]
+      const site = pgnHeader.match(/(?<=Site.").*(?=")/)[0]
+      const date = pgnHeader.match(/(?<=Date.").*(?=")/)[0]
+      const round = pgnHeader.match(/(?<=Round.").*(?=")/)[0]
+      const white = pgnHeader.match(/(?<=White.").*(?=")/)[0]
+      const black = pgnHeader.match(/(?<=Black.").*(?=")/)[0]
+      const result = pgnHeader.match(/(?<=Result.").*(?=")/)[0]
+      const eco = pgnHeader.match(/(?<=ECO.").*(?=")/)[0]
+      const whiteElo = pgnHeader.match(/(?<=WhiteElo.").*(?=")/)[0]
+      const blackElo = pgnHeader.match(/(?<=BlackElo.").*(?=")/)[0]
+      const plyCount = pgnHeader.match(/(?<=PlyCount.").*(?=")/)[0]
+      const eventDate = pgnHeader.match(/(?<=EventDate.").*(?=")/)[0]
+      const source = pgnHeader.match(/(?<=Source.").*(?=")/)[0]
+      const loadedConfig = {
+        width: '300px',
+        resizable: false,
+        position: 'start',
+        showCoords: false,
+        orientation: 'white',
+        theme: 'default',
+        pieceStyle: 'wikipedia',
+        figurine: '',
+        locale: 'en',
+        timerTime: '',
+        layout: 'top',
+        showFen: false,
+        coordsInner: true,
+        headers: true,
+        coordsFactor: '1.0',
+        coordsFontSize: '',
+        colorMarker: '',
+        startPlay: '',
+        hideMovesBefore: true,
+        notation: 'short',
+        notationLayout: 'inline',
+        pgn: pgnData,
+      }
+      res.render('game', {
+        config: loadedConfig,
+        event: event,
+        site: site,
+        date: date,
+        round: round,
+        white: white,
+        black: black,
+        result: result,
+        eco: eco,
+        whiteElo: whiteElo,
+        blackElo: blackElo,
+        plyCount: plyCount,
+        eventDate: eventDate,
+        source: source,
+      })
+    }
+    catch (err) {  // handle this error more gracefully. Send an error message.
+      res.redirect('/game')
+    }
+  }
+});
+
+// http error reponse codes
+app.use((req, res) => res.status(404).send('404'))
+app.use((req, res) => res.status(500).send('500'))
+
+app.listen(port, () => {
+  console.log(`Server listening on the port::${port}`);
+});
